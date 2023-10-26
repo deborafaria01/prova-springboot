a3f50ea1d4b1f10dc6c5ccbdc0b1c33591aac4b8
Debora Faria <deeborafaria@gmail.com>
2023-10-26 12:06:04 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/provaspringboot/ProvaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.provaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/provaspringboot/ProvaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.provaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
