4bef0a9fe72fad6c48b0aeb498cb653083947dd7
Debora Faria <deeborafaria@gmail.com>
2023-10-26 15:23:08 +0000

    Update

---
+++ src/main/java/br/gov/sp/fatec/provaspringboot/entity/Turma.java
@@ -0,0 +1,10 @@
+package br.gov.sp.fatec.provaspringboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tr_turma")
+public class Turma {
+    
+}
