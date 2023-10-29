6507f81e164277bff77a4a3c6e155af9c8006a94
Debora Faria <deeborafaria@gmail.com>
2023-10-29 22:02:35 +0000

    Criação de rotas

--- src/main/java/br/gov/sp/fatec/provaspringboot/controller/TurmaController.java
+++ src/main/java/br/gov/sp/fatec/provaspringboot/controller/TurmaController.java
@@ -2,13 +2,15 @@ package br.gov.sp.fatec.provaspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import br.gov.sp.fatec.provaspringboot.entity.Turma;
 import br.gov.sp.fatec.provaspringboot.service.ITurmaService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @CrossOrigin
@@ -21,6 +23,16 @@ public class TurmaController {
     @PostMapping
     public Turma novaTurma (@RequestBody Turma turma){
         return service.novaTurma(turma);
+    }
+
+    @GetMapping
+    public List<Turma> buscarTodas(){
+        return service.buscarTodas();
+    }
+
+    @GetMapping(value = "/ano") //subrota
+    public List<Turma> buscarPorRangeDeAno(@RequestParam("anoInicio") Integer anoInicio, @RequestParam("anoFim") Integer anoFim){
+        return service.buscarPorRangeDeAno(anoInicio, anoFim);
     }
 
     
